9b5206be032eff2fb39a146cf6cb86859e19c4b8
tuagkcuf <jokersms222@gmail.com>
2023-05-13 17:32:35 +0300

    upgrade package.json, add dependencies and dev dependencies

---
+++ contracts/AirDrop.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
+
+contract AirDrop is ERC20("AirDrop", "AD") {
+    
+}
